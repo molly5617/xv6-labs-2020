fe84572e369ebcea51feca88ccf338e5676f371a
molly5617 <mollytsai5617@gmail.com>
2025-01-31 08:00:23 +0000

    123

--- kernel/vm.c
+++ kernel/vm.c
@@ -460,6 +460,26 @@ int kama_pgtblprint(pagetable_t pagetable, int depth)
   for (int i = 0; i < 512; i++)
   {
     pte_t pte = pagetable[i];
+    if (pte & PTE_V)
+    {
+      printf("..");
+      for (int j = 0; j < depth; ++j)
+      {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0)
+      {
+        uint64 child = PTE2PA(pte);
+        kama_pgtblprint((pagetable_t)child, depth + 1);
+      }
+    }
   }
   return 0;
+}
+
+int kama_vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  return kama_pgtblprint(pagetable, 0);
 }
